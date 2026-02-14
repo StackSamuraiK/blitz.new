83e2557a5aeeaf45a95b4fd076e27539028f96a1
Kshitiz Prakash <kshitizsrivastav3344@gmail.com>
2026-02-15 00:38:47 +0530

    added vercel analytics

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import {Home} from "./pages/Home"
 import { Builder } from './pages/Builder';
+import { Analytics } from "@vercel/analytics/next"
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/builder" element={<Builder />} />
+        <Analytics />
       </Routes>
     </BrowserRouter>
   );
